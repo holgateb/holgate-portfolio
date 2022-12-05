1a86e2770c0678454cf4e312b9c15f3cec3b8af4
Ben Holgate <holgateb@hotmail.com>
2022-12-04 18:38:30 -0800

    created all components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Banner />
+        <Skills />
+        <Footer />
       </header>
     </div>
   );
