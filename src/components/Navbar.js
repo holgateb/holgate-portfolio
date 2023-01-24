29e6200d5e80a21eade895c0164b1f6d22c8079d
Ben Holgate <holgateb@hotmail.com>
2023-01-24 13:04:15 -0800

    updated navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,6 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
